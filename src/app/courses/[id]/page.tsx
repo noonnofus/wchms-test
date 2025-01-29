6212223041a2f2b1dcd6d7f441e085285d08a73b
achen930 <annabellechen101@gmail.com>
2025-01-29 12:35:51 -0800

    render course details based on id

--- src/app/courses/[id]/page.tsx
+++ src/app/courses/[id]/page.tsx
@@ -1,8 +1,34 @@
 "use client";
 import TabsMenu from "@/components/shared/tabs-menu";
 import CourseDetailsCard from "@/components/courses/course-details-card";
+import { useParams } from "next/navigation";
 
 export default function Home() {
+    const { id } = useParams();
+
+    const myCourses = [
+        {
+            id: "1",
+            name: "Course 122",
+            image: "/course-image.png",
+            imgAlt: "A Snake",
+            description:
+                "2025年は、60年に一度巡ってくる「乙巳（きのと・み）」の年。乙巳の年は、新しいものが生まれ、成長していく年と言われています。第19期となる脳の運動教室も、今年は「シン 脳の運動教室」としてブラッシュアップしていきます！皆さま、本年もよろしくお願いいたします。",
+        },
+        {
+            id: "2",
+            name: "Course 123",
+            image: "/course-image.png",
+            imgAlt: "A Snake",
+            description: "Course Description",
+        },
+    ];
+
+    const course = myCourses.find((course) => course.id === id); // find course based on course id
+    if (!course) {
+        return <div>No course found</div>;
+    }
+
     return (
         <div>
             <TabsMenu
@@ -10,8 +36,8 @@ export default function Home() {
                 rightLabel="Course Exercises"
                 leftChildren={
                     <CourseDetailsCard
-                        name="第19期：脳の運動教室(シン 脳の運動教室)"
-                        description="2025年は、60年に一度巡ってくる「乙巳（きのと・み）」の年。乙巳の年は、新しいものが生まれ、成長していく年と言われています。第19期となる脳の運動教室も、今年は「シン 脳の運動教室」としてブラッシュアップしていきます！皆さま、本年もよろしくお願いいたします。"
+                        name={course.name}
+                        description={course?.description}
                     />
                 }
                 rightChildren={<></>}
