41e58a3f534d94a93a9fe2ec147a8bbea2506a39
JustArmaan <singharmaanbrar@gmail.com>
2025-01-28 16:57:44 -0800

    WCHMS-27 fix assignments reference

--- src/db/schema/announcement.ts
+++ src/db/schema/announcement.ts
@@ -41,27 +41,3 @@ export const announcements = mysqlTable("announcements", {
     sortOrder: int("sort_order").default(0),
     userId: varchar("user_id", { length: 255 }).notNull(),
 });
-
-export const users = mysqlTable("users", {
-    id: serial("id").primaryKey(),
-    firstName: varchar("first_name", { length: 255 }).notNull(),
-    lastName: varchar("last_name", { length: 255 }).notNull(),
-    email: varchar("email", { length: 255 }).notNull(),
-    birthDate: date("birth_date"),
-    gender: mysqlEnum("gender", ["male", "female", "other"] as const).notNull(),
-    building: varchar("building", { length: 255 }),
-    street: varchar("street", { length: 255 }),
-    city: varchar("city", { length: 255 }).notNull(),
-    province: varchar("province", { length: 255 }).notNull(),
-    country: varchar("country", { length: 255 }).notNull(),
-    postalCode: varchar("postal_code", { length: 255 }),
-    timezone: varchar("timezone", { length: 255 }).default("America/Vancouver"),
-    lang: varchar("lang", { length: 255 }).default("en"),
-    role: mysqlEnum("role", ["client", "staff", "admin"] as const).default(
-        "client"
-    ),
-    status: mysqlEnum("status", ["active", "inactive"] as const).default(
-        "active"
-    ),
-    courseId: varchar("course_id", { length: 255 }),
-});
--- src/db/schema/assignment.ts
+++ src/db/schema/assignment.ts
@@ -1,62 +1,33 @@
 import {
-    date,
+    boolean,
     int,
     mysqlEnum,
     mysqlTable,
     serial,
-    timestamp,
+    text,
     varchar,
 } from "drizzle-orm/mysql-core";
-import { Course } from "./course";
 
-enum AssignmentKind {
+export enum AssignmentKind {
     homework = "homework",
     report = "report",
+    project = "project",
 }
 
 const assignmentKindEnumValues = [
     AssignmentKind.homework,
     AssignmentKind.report,
+    AssignmentKind.project,
 ] as const;
 
 export const assignments = mysqlTable("assignments", {
     id: serial("id").primaryKey(),
     title: varchar("title", { length: 255 }).notNull(),
     kind: mysqlEnum("kind", assignmentKindEnumValues).notNull(),
-    description: varchar("description", { length: 255 }),
-    internalNote: varchar("internal_note", { length: 255 }),
+    description: text("description"),
+    internalNote: text("internal_note"),
     uploadId: varchar("upload_id", { length: 255 }),
-    courseId: varchar("course_id", { length: 255 })
-        .notNull()
-        .references(() => Course.id),
-    sortOrder: int("sort_order").default(0),
-    createdAt: timestamp("created_at").defaultNow(),
-    updatedAt: timestamp("updated_at").defaultNow().onUpdateNow(),
-});
-
-export const users = mysqlTable("users", {
-    id: serial("id").primaryKey(),
-    firstName: varchar("first_name", { length: 255 }).notNull(),
-    lastName: varchar("last_name", { length: 255 }).notNull(),
-    email: varchar("email", { length: 255 }).notNull(),
-    birthDate: date("birth_date"),
-    gender: mysqlEnum("gender", ["male", "female", "other"] as const).notNull(),
-    status: mysqlEnum("status", ["active", "inactive"] as const).default(
-        "active"
-    ),
-    role: mysqlEnum("role", ["client", "staff", "admin"] as const).default(
-        "client"
-    ),
-    courseId: varchar("course_id", { length: 255 }),
-});
-
-export const courseParticipants = mysqlTable("course_participants", {
-    id: serial("id").primaryKey(),
-    userId: varchar("user_id", { length: 255 })
-        .notNull()
-        .references(() => users.id),
-    courseId: varchar("course_id", { length: 255 })
-        .notNull()
-        .references(() => Course.id),
-    role: mysqlEnum("role", ["student", "teacher"] as const).notNull(),
+    courseId: varchar("course_id", { length: 255 }).notNull(),
+    isAggregated: boolean("is_aggregated").default(false),
+    sortOrder: int("sort_order").notNull(),
 });
