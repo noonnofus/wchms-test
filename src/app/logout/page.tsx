011120f0022f7c45a6f4d16e8599b9e7b53e8a66
Angus Ng <ang183@my.bcit.ca>
2025-02-06 08:19:01 -0800

    implement frontend of logout, add tailwind styling for destructive actions

---
+++ src/app/logout/page.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
+import Link from "next/link";
+
+export default function Logout() {
+    return (
+        <div className="flex flex-col items-center gap-8 h-full">
+            <h1 className="mt-4 text-[32px] font-semibold">Logout</h1>
+            <Card className="flex flex-col w-full gap-2 items-center text-center py-8 px-4 h-auto">
+                <p className="text-xl leading-4 font-semibold">
+                    Are you sure you want to
+                </p>
+                <p className="text-xl font-semibold">LOG OUT?</p>
+                <p className="text-xl font-semibold mt-8">
+                    You will need to LOG IN again to access your account.
+                </p>
+            </Card>
+            <div className="w-full flex flex-col gap-4">
+                <form
+                    method="POST"
+                    action="/api/auth/logout"
+                    className="w-full"
+                >
+                    <Button
+                        type="submit"
+                        className="bg-destructive-red hover:bg-destructive-hover text-destructive-text rounded-full w-full font-semibold text-base min-h-[45px]"
+                    >
+                        Logout
+                    </Button>
+                </form>
+                <Button
+                    asChild
+                    variant="outline"
+                    className="border-primary-green hover:bg-primary-green text-primary-green rounded-full w-full font-semibold text-base hover:text-white min-h-[45px]"
+                >
+                    <Link href="/">Cancel</Link>
+                </Button>
+            </div>
+        </div>
+    );
+}
