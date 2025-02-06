1ac7fc62148f1d2cfd537567a490eea3692f07cd
Angus Ng <ang183@my.bcit.ca>
2025-02-06 08:40:17 -0800

    update cta for joining session, begin updates to course materials

--- src/app/courses/[id]/page.tsx
+++ src/app/courses/[id]/page.tsx
@@ -15,6 +15,33 @@ export default function Home() {
             imgAlt: "A Snake",
             description:
                 "2025年は、60年に一度巡ってくる「乙巳（きのと・み）」の年。乙巳の年は、新しいものが生まれ、成長していく年と言われています。第19期となる脳の運動教室も、今年は「シン 脳の運動教室」としてブラッシュアップしていきます！皆さま、本年もよろしくお願いいたします。",
+            materials: [
+                {
+                    title: "Week 1: A really long title to see how it would look with multiple lines",
+                    content: "Some description",
+                    createdAt: new Date(1738859540),
+                    file: "Week1.pdf",
+                },
+                {
+                    title: "Week 2",
+                    content:
+                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id enim eget sem maximus accumsan. Pellentesque id varius mi, non sollicitudin orci. Donec eu condimentum justo. Donec vel sapien arcu. Quisque dapibus ligula non imperdiet malesuada.",
+                    createdAt: new Date(1738859545),
+                    file: "Week2.pdf",
+                },
+                {
+                    title: "Week 3",
+                    content: "No review materials this week",
+                    createdAt: new Date(1738859550),
+                    file: null,
+                },
+                {
+                    title: "Week 4: Just a file",
+                    content: null,
+                    createdAt: new Date(1738859550),
+                    file: "Week4.pdf",
+                },
+            ],
         },
         {
             id: "2",
@@ -22,6 +49,7 @@ export default function Home() {
             image: "/course-image.png",
             imgAlt: "A Snake",
             description: "Course Description",
+            materials: [],
         },
     ];
 
@@ -34,7 +62,7 @@ export default function Home() {
         <div>
             <TabsMenu
                 leftLabel="Course Home"
-                rightLabel="Course Exercises"
+                rightLabel="Course Materials"
                 leftChildren={
                     <CourseDetailsCard
                         name={course.name}
