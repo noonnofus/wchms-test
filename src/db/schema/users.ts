b8edb06763065516316ff5d31c4c7e157b98ba72
achen930 <annabellechen101@gmail.com>
2025-02-13 19:47:19 -0800

    update user schema so id uses int

--- src/db/schema/users.ts
+++ src/db/schema/users.ts
@@ -2,12 +2,12 @@ import {
     mysqlEnum,
     mysqlTable,
     varchar,
-    serial,
+    int,
     date,
 } from "drizzle-orm/mysql-core";
 
 export const users = mysqlTable("users", {
-    id: serial("id").primaryKey(),
+    id: int("id").primaryKey().autoincrement().notNull(),
     firstName: varchar("first_name", { length: 255 }).notNull(),
     lastName: varchar("last_name", { length: 255 }).notNull(),
     email: varchar("email", { length: 255 }).unique(),
