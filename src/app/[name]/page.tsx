8e580acf002d98228f5ba3c3e5fc7149862ce03c
Angus Ng <ang183@my.bcit.ca>
2025-02-15 15:21:33 -0800

    participant login

--- src/app/[name]/page.tsx
+++ src/app/[name]/page.tsx
@@ -1,11 +1,39 @@
 "use client";
-import { useParams } from "next/navigation";
+
+import { useParams, useRouter } from "next/navigation";
+import { useState } from "react";
+import { signIn } from "next-auth/react";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 export default function ParticipantConfirmation() {
+    const router = useRouter();
     const { name } = useParams();
+    const [lastName, setLastName] = useState("");
+    const [error, setError] = useState("");
+
+    const handleSignIn = async () => {
+        if (!lastName) {
+            setError("Please enter your last name.");
+            return;
+        }
+        console.log(name, lastName);
+
+        const result = await signIn("credentials", {
+            callbackUrl: "/Landing",
+            firstName: name,
+            lastName: lastName,
+            loginType: "participant",
+            redirect: false,
+        });
+        if (result?.error) {
+            setError("Invalid Last Name. Please try again.");
+        } else {
+            router.push("/landing");
+        }
+    };
+
     return (
         <div className="flex flex-col gap-20 w-full h-full items-center justify-center">
             <h1 className="font-semibold text-4xl">Welcome back, {name}!</h1>
@@ -17,14 +45,17 @@ export default function ParticipantConfirmation() {
                     type="text"
                     placeholder="Last Name"
                     className="py-6"
-                ></Input>
+                    value={lastName}
+                    onChange={(e) => setLastName(e.target.value)}
+                />
+                {error && <p className="text-red-500">{error}</p>}
             </div>
             <div className="flex flex-row gap-4 w-full justify-between">
                 <Button
-                    asChild
+                    onClick={handleSignIn}
                     className="w-full h-full rounded-full bg-primary-green hover:bg-[#045B47] font-semibold text-xl py-4"
                 >
-                    <Link href="/landing">Enter Site</Link>
+                    Enter Site
                 </Button>
                 <Button
                     asChild
--- src/auth.ts
+++ src/auth.ts
@@ -1,9 +1,10 @@
 import db from "@/db";
 import { users } from "@/db/schema/users";
 import bcrypt from "bcrypt";
-import { eq } from "drizzle-orm";
+import { eq, and } from "drizzle-orm";
 import NextAuth, { AuthOptions, DefaultSession } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
+import { participants } from "./db/schema/participants";
 
 declare module "next-auth" {
     interface Session {
@@ -19,32 +20,84 @@ export const authConfig: AuthOptions = {
             credentials: {
                 email: { label: "Email", type: "email" },
                 password: { label: "Password", type: "password" },
+                firstName: {
+                    label: "First Name",
+                    type: "text",
+                    placeholder: "Enter your first name",
+                },
+                lastName: {
+                    label: "Last Name",
+                    type: "text",
+                    placeholder: "Enter your last name",
+                },
+                loginType: {
+                    label: "Login Type",
+                    type: "text",
+                    placeholder: "admin or participant",
+                },
             },
             async authorize(credentials) {
-                if (!credentials?.email || !credentials?.password) {
-                    throw new Error("Enter your email and password");
+                if (credentials?.loginType.toLowerCase() === "admin") {
+                    if (!credentials?.email || !credentials?.password) {
+                        throw new Error("Enter your email and password");
+                    }
+
+                    const user = await db
+                        .select()
+                        .from(users)
+                        .where(eq(users.email, credentials.email))
+                        .limit(1)
+                        .then((rows) => rows[0]);
+
+                    if (!user || !user.password) {
+                        throw new Error("Invalid email or password");
+                    }
+
+                    const validPassword = await bcrypt.compare(
+                        credentials.password,
+                        user.password
+                    );
+                    if (!validPassword) {
+                        throw new Error("Invalid password");
+                    }
+
+                    return {
+                        id: user.id.toString(),
+                        email: user.email,
+                        role: user.role,
+                    };
+                } else if (
+                    credentials?.loginType.toLowerCase() === "participant"
+                ) {
+                    if (!credentials?.firstName || !credentials?.lastName) {
+                        throw new Error("Enter your first name, and last name");
+                    }
+
+                    const user = await db
+                        .select()
+                        .from(participants)
+                        .where(
+                            and(
+                                eq(
+                                    participants.firstName,
+                                    credentials.firstName
+                                ),
+                                eq(participants.lastName, credentials.lastName)
+                            )
+                        )
+                        .limit(1)
+                        .then((rows) => rows[0]);
+                    if (!user) {
+                        throw new Error("Invalid user");
+                    }
+
+                    return {
+                        id: user.id.toString(),
+                        firstName: user.firstName,
+                        lastName: user.lastName,
+                    };
                 }
-
-                const user = await db
-                    .select()
-                    .from(users)
-                    .where(eq(users.email, credentials.email))
-                    .limit(1)
-                    .then((rows) => rows[0]);
-
-                if (!user || !user.password) {
-                    throw new Error("Invalid email or password");
-                }
-
-                const validPassword = await bcrypt.compare(
-                    credentials.password,
-                    user.password
-                );
-                if (!validPassword) {
-                    throw new Error("Invalid password");
-                }
-
-                return { id: user.id, email: user.email, role: user.role };
+                throw new Error("Invalid signin method");
             },
         }),
     ],
@@ -81,7 +134,8 @@ export const authConfig: AuthOptions = {
     },
 
     pages: {
-        signIn: "/admin/logIn",
+        signIn: undefined,
+        signOut: "/logout",
     },
 
     secret: process.env.NEXTAUTH_SECRET,
