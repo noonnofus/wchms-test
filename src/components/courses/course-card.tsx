60f5933841681b0b33ba827271bd45a74c65bddb
achen930 <annabellechen101@gmail.com>
2025-02-18 17:45:45 -0800

    fix: made entire course card clickable

--- src/components/courses/course-card.tsx
+++ src/components/courses/course-card.tsx
@@ -34,100 +34,105 @@ interface AdminVariantProps extends CourseCardProps {
 export default function CourseCard(
     props: ClientVariantProps | AdminVariantProps
 ) {
+    const courseLink =
+        props.variant === "admin"
+            ? `/admin/courses/${props.id}`
+            : `/courses/${props.id}`;
     return (
-        <div className="flex flex-col items-center">
-            <Card
-                className={twMerge(
-                    "w-full pb-4",
-                    props.variant == "admin" ? "relative" : ""
-                )}
-            >
-                <CardHeader>
-                    <CardTitle
-                        className={twMerge(
-                            props.variant === "admin"
-                                ? "px-6 sm:px-4 md:px-4"
-                                : ""
-                        )}
-                    >
-                        {props.name}
-                    </CardTitle>
-                </CardHeader>
-                {props.image && (
-                    <Image
-                        src={props.image}
-                        width={200}
-                        height={200}
-                        alt={props.imageAlt || `${props.name} Course Image`}
-                        className="mb-2"
-                    />
-                )}
-                {props.variant == "client" &&
-                    (props.enrolled ? (
-                        <Button
-                            asChild
-                            className="bg-primary-green hover:bg-[#045B47] text-white rounded-full w-full font-semibold text-base"
+        <div className="flex flex-col items-center justify-center relative">
+            <Card className="w-full relative">
+                <Link
+                    href={courseLink}
+                    className="flex flex-col gap-2 items-center w-full h-full p-4"
+                >
+                    <CardHeader>
+                        <CardTitle
+                            className={twMerge(
+                                props.variant === "admin"
+                                    ? "px-6 sm:px-4 md:px-4"
+                                    : ""
+                            )}
                         >
-                            <Link
-                                href="/courses/[id]"
-                                as={`/courses/${props.id}`}
-                            >
-                                View Course
-                            </Link>
-                        </Button>
-                    ) : (
-                        <>
+                            {props.name}
+                        </CardTitle>
+                    </CardHeader>
+                    {props.image && (
+                        <Image
+                            src={props.image}
+                            width={200}
+                            height={200}
+                            alt={props.imageAlt || `${props.name} Course Image`}
+                        />
+                    )}
+                    {props.variant == "client" &&
+                        (props.enrolled ? (
                             <Button
                                 asChild
-                                className="hover:bg-primary-green border-primary-green text-primary-green hover:text-white rounded-full w-full font-semibold text-base"
-                                variant="outline"
+                                className="bg-primary-green hover:bg-[#045B47] text-white rounded-full w-full font-semibold text-base"
                             >
-                                <Link
-                                    href="/courses/[id]"
-                                    as={`/courses/${props.id}`}
-                                >
-                                    Details
+                                <Link href={`/courses/${props.id}`}>
+                                    View Course
                                 </Link>
                             </Button>
-                            <Button
-                                asChild
-                                className="mt-2 bg-primary-green hover:bg-[#045B47] text-white rounded-full w-full font-semibold text-base"
-                            >
-                                <Link href="#">Enroll</Link>
-                            </Button>
-                        </>
-                    ))}
-                {props.variant == "admin" && (
-                    <div className="flex justify-between items-center">
+                        ) : (
+                            <>
+                                <Button
+                                    asChild
+                                    className="hover:bg-primary-green border-primary-green text-primary-green hover:text-white rounded-full w-full font-semibold text-base"
+                                    variant="outline"
+                                >
+                                    <Link href={`/courses/${props.id}`}>
+                                        Details
+                                    </Link>
+                                </Button>
+                                <Button
+                                    asChild
+                                    className="mt-2 bg-primary-green hover:bg-[#045B47] text-white rounded-full w-full font-semibold text-base"
+                                >
+                                    <Link href="#">Enroll</Link>
+                                </Button>
+                            </>
+                        ))}
+                    {props.variant == "admin" && (
                         <CardContent>
                             <p>{props.description}</p>
                         </CardContent>
-                        <button
-                            onClick={() =>
-                                props.handleEditButtonClick(props.id)
-                            }
+                    )}
+                </Link>
+
+                {props.variant == "admin" && (
+                    <button
+                        onClick={(e) => {
+                            e.stopPropagation();
+                            props.handleEditButtonClick(props.id);
+                        }}
+                        className="absolute right-5 top-4"
+                    >
+                        <EditIcon />
+                    </button>
+                )}
+
+                {props.variant == "admin" && (
+                    <Link
+                        href={`/admin/courses/${props.id}`}
+                        className="absolute right-2 top-1/2 transform -translate-y-1/2"
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="50"
+                            height="48"
+                            viewBox="0 0 50 48"
+                            fill="none"
                         >
-                            <EditIcon className="absolute right-[2%] top-[8%]" />
-                        </button>
-                        <Link href={`/admin/courses/${props.id}`}>
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="50"
-                                height="48"
-                                viewBox="0 0 50 48"
-                                fill="none"
-                                className="absolute right-0 top-1/2 transform -translate-y-1/2"
-                            >
-                                <path
-                                    d="M19.1364 10L33.4546 24L19.1364 38"
-                                    stroke="black"
-                                    strokeWidth="4"
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                />
-                            </svg>
-                        </Link>
-                    </div>
+                            <path
+                                d="M19.1364 10L33.4546 24L19.1364 38"
+                                stroke="black"
+                                strokeWidth="4"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                            />
+                        </svg>
+                    </Link>
                 )}
             </Card>
         </div>
