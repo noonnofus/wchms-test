c9ea5a159a165721012362d7bde3493d267db68b
Angus Ng <ang183@my.bcit.ca>
2025-02-28 16:31:49 -0800

    add permission check for viewing list of admin/staff

--- src/app/api/admin/route.ts
+++ src/app/api/admin/route.ts
@@ -1,7 +1,21 @@
+import { authConfig } from "@/auth";
 import { getAllAdmins } from "@/db/queries/admins";
+import { validateAdminOrStaff } from "@/lib/validation";
+import { getServerSession } from "next-auth";
 
 export async function GET() {
     try {
+        const session = await getServerSession(authConfig);
+
+        //Only admin or staff can get a list of all admin/staff
+        if (!validateAdminOrStaff(session)) {
+            return new Response(
+                JSON.stringify({
+                    error: "Unauthorized: insufficient permissions",
+                }),
+                { status: 401 }
+            );
+        }
         const admins = await getAllAdmins();
         return new Response(JSON.stringify(admins), {
             status: 200,
